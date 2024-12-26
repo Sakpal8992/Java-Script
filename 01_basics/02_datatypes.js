c6632d07134b025d86bbaf6e4eda94e31522bc65
Sakpal8992 <sakpal.vaibhav92@gmail.com>
2024-12-26 18:07:01 +0000

    commit changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, no browser
+
+console.log(3 
+    + 
+    3)  // code readablity should be high
+
+console.log("vaibhav")
+
+
+let name = "vaibhav"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number ==> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stanalone value
+// undefined =>
+// symbol => to define unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
